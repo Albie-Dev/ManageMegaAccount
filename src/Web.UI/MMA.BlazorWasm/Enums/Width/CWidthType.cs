0ae323631fb3d0608509c1a8f968d65a7cddabc2
Albie-Dev <albie.dev.0403@gmail.com>
2025-03-11 17:28:43 +0700

    Thêm table component template

---
+++ src/Web.UI/MMA.BlazorWasm/Enums/Width/CWidthType.cs
@@ -0,0 +1,19 @@
+namespace MMA.BlazorWasm
+{
+    public enum CWidthType
+    {
+        None = 0,
+        Col1 = 1,
+        Col2 = 2,
+        Col3 = 3,
+        Col4 = 4,
+        Col5 = 5,
+        Col6 = 6,
+        Col7 = 7,
+        Col8 = 8,
+        Col9 = 9,
+        Col10 = 10,
+        Col11 = 11,
+        Col12 = 12
+    }
+}
