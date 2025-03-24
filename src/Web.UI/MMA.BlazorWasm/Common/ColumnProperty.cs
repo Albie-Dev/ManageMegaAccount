fd4f819163c3fb0adfe7ef6608f6490849381cb7
Albie-Dev <albie.dev.0403@gmail.com>
2025-03-24 07:07:00 +0700

    Xây dựng trang Paging và Create cho Actor

---
+++ src/Web.UI/MMA.BlazorWasm/Common/ColumnProperty.cs
@@ -0,0 +1,9 @@
+namespace MMA.BlazorWasm
+{
+    public class ColumnProperty
+    {
+        public string ColumnName { get; set; } = string.Empty;
+        public string PropertyName { get; set; } = string.Empty;
+        public bool IsSort { get; set; } = false;
+    }
+}
