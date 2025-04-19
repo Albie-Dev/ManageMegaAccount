0faf15367740062d5ed6af2a23c6721da923f662
Albie-Dev <albie.dev.0403@gmail.com>
2025-04-19 23:09:40 +0700

    Xây dựng base combobox hỗ trợ các type Select, Checkbox, Radio

---
+++ src/Web.UI/MMA.BlazorWasm/Components/Common/Combobox/ComboboxList.razor.cs
@@ -0,0 +1,105 @@
+
+using Microsoft.AspNetCore.Components;
+using MMA.Domain;
+
+namespace MMA.BlazorWasm.Components.Common.Combobox
+{
+    public partial class ComboboxList<T>
+    {
+        [Parameter] public string ClassName { get; set; } = string.Empty;
+        [Parameter] public string PlaceHolderName { get; set; } = string.Empty;
+        [Parameter] public CComboboxModeType Mode { get; set; } = CComboboxModeType.Select;
+
+        [Parameter] public List<CComboboxModel> Items { get; set; } = new();
+
+        [Parameter] public T? BindValue { get; set; }
+        [Parameter] public EventCallback<T> BindValueChanged { get; set; }
+
+        [Parameter] public List<T> BindValues { get; set; } = new();
+        [Parameter] public EventCallback<List<T>> BindValuesChanged { get; set; }
+
+        public List<CComboboxModel> FilteredOptions { get; set; } = new();
+        public List<CComboboxModel> SelectedOptions { get; set; } = new();
+        public HashSet<T> SelectedValues { get; set; } = new();
+
+        public bool ShowDropdown { get; set; }
+        public string SearchFilter { get; set; } = string.Empty;
+        public string RadioGroupName { get; set; } = $"RadioGroup_{Guid.NewGuid()}";
+
+        protected override void OnInitialized()
+        {
+            FilteredOptions = Items.ToList();
+
+            if (Mode == CComboboxModeType.Checkbox)
+            {
+                SelectedValues = new HashSet<T>(BindValues ?? []);
+            }
+            else if (BindValue != null)
+            {
+                SelectedValues = new HashSet<T> { BindValue };
+            }
+
+            SelectedOptions = Items.Where(i => i.Value != null && SelectedValues.Contains((T)i.Value)).ToList();
+        }
+
+        protected override void OnParametersSet()
+        {
+            if (Mode == CComboboxModeType.Checkbox)
+            {
+                SelectedValues = new HashSet<T>(BindValues ?? []);
+            }
+            else if (BindValue != null)
+            {
+                SelectedValues = new HashSet<T> { BindValue };
+            }
+
+            SelectedOptions = Items.Where(i => i.Value != null && SelectedValues.Contains((T)i.Value)).ToList();
+        }
+
+        public void ToggleDropdown() => ShowDropdown = !ShowDropdown;
+
+        public async Task HandleRadioSelection(CComboboxModel option)
+        {
+            SelectedValues.Clear();
+            SelectedValues.Add((T)option.Value!);
+            SelectedOptions = new() { option };
+            ShowDropdown = false;
+
+            await BindValueChanged.InvokeAsync((T)option.Value!);
+            StateHasChanged();
+        }
+
+        public async Task HandleSelectOption(CComboboxModel option)
+        {
+            SelectedValues.Clear();
+            SelectedValues.Add((T)option.Value!);
+            SelectedOptions = new() { option };
+            ShowDropdown = false;
+
+            await BindValueChanged.InvokeAsync((T)option.Value!);
+            StateHasChanged();
+        }
+
+        public async Task HandleCheckboxSelection(CComboboxModel option, bool isChecked)
+        {
+            var value = (T)option.Value!;
+            if (isChecked)
+                SelectedValues.Add(value);
+            else
+                SelectedValues.Remove(value);
+
+            SelectedOptions = Items.Where(i => SelectedValues.Contains((T)i.Value!)).ToList();
+            await BindValuesChanged.InvokeAsync(SelectedValues.ToList());
+            StateHasChanged();
+        }
+
+        public void FilterOptions(ChangeEventArgs e)
+        {
+            SearchFilter = e.Value?.ToString() ?? "";
+            FilteredOptions = string.IsNullOrWhiteSpace(SearchFilter)
+                ? Items.ToList()
+                : Items.Where(o => o.Name.Contains(SearchFilter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+
+}
---
+++ src/Web.UI/MMA.BlazorWasm/Enums/CComboboxModeType.cs
@@ -0,0 +1,9 @@
+namespace MMA.BlazorWasm
+{
+    public enum CComboboxModeType
+    {
+        Select = 1,
+        Radio = 2,
+        Checkbox = 3
+    }
+}
