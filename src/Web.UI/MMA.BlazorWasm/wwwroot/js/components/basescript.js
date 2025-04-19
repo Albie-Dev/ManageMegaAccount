30c00306e172ee6f8c2f305cef8cc63b80869c65
Albie-Dev <albie.dev.0403@gmail.com>
2025-04-19 23:14:24 +0700

    Hỗ trợ download excel file

---
+++ src/Web.UI/MMA.BlazorWasm/wwwroot/js/components/basescript.js
@@ -0,0 +1,8 @@
+window.downloadFile = (fileName, base64Data) => {
+    console.log(fileName);
+    var link = document.createElement('a');
+    link.href = 'data:application/octet-stream;base64,' + base64Data;
+    link.download = fileName;
+    console.log(link.download);
+    link.click();
+};
